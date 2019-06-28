e71b5e2dd21caf33078a19302de1fd1f10157497
Peter <petergorman11@gmail.com>
2019-06-28 10:55:37 -0400

    Add event callbacks to load, update, and draw to allow main to still perform

--- main.lua
+++ main.lua
@@ -1,4 +1,14 @@
 require("lib.loader")
 
+Events.Listen("Load", "Pong.Load", function()
+    Print("Game loading")
+end)
 
+Events.Listen("Update", "Pong.Update", function(dt)
+    --print("Game updating... (" .. dt .. ")")
+end)
 
+Events.Listen("Draw", "Pong.Draw", function()
+    --print("Game drawing...")
+    love.graphics.print(love.timer.getFPS() .. "FPS", 5, 5)
+end)
